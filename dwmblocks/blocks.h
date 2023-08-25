8ad71e60c16ee6f64919bed6e5190d1782e5b051
javiermoralesdev <javiermoralesdev@gmail.com>
2023-08-25 12:39:48 +0200

    Added my configs

--- dwmblocks/blocks.h
+++ dwmblocks/blocks.h
@@ -1,10 +1,8 @@
 static const Block blocks[] = {
-//   Icon    Command                          Update Interval     Update Signal
-    { "  ", "checkupdates | wc -l",                 60,               0 },
-    { "",    "brightness",                           2,                0 },
-    { "",    "volume",                               2,                0 },
-    { "",    "battery",                              60,               0 },
-    { "",    "date '+ %d/%m/%Y   %H:%M%p'",        5,                0 },
+    //   Icon    Command                          Update Interval     Update Signal
+    {"  ", "checkupdates | wc -l", 60, 0},
+    {"", " ~/.local/bin/volume %", 2, 0},
+    {"", "date '󰃭 %d/%m/%Y   %H:%M%p'", 5, 0},
 };
 
 // Sets delimeter between status commands. NULL character ('\0') means no delimeter.
