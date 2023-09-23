b7ed0a5bb144f7236e88c16b89862238f295f7ee
javiermoralesdev <javiermoralesdev@gmail.com>
2023-09-23 13:13:06 +0200

    Modified the blocks

--- dwmblocks/blocks.h
+++ dwmblocks/blocks.h
@@ -1,6 +1,5 @@
 static const Block blocks[] = {
     //   Icon    Command                          Update Interval     Update Signal
-    {"  ", "bash ~/.local/bin/version", 60, 0},
     {"  ", "checkupdates | wc -l", 1, 0},
     {"", "~/.local/bin/volume", 1, 0},
     {"", "date +'󰃭 %d/%m/%Y   %H:%M%p'", 1, 0},
