51e97b23adffc79c0c46b42ca5be4cca314d7b10
javiermoralesdev <javiermoralesdev@gmail.com>
2023-10-01 11:58:02 +0200

    Added a font

--- config.h
+++ config.h
@@ -157,7 +157,7 @@ static Key keys[] = {
     {MODKEY | ShiftMask, XK_f, togglefloating, {0}},
 
     // Toggle bar
-    {MODKEY | ShiftMask, XK_h, togglebar, {0}},
+    //{MODKEY | ShiftMask, XK_h, togglebar, {0}},
 
     // Move windows in current stack
     {MODKEY | ShiftMask, XK_j, rotatestack, {.i = +1}},
